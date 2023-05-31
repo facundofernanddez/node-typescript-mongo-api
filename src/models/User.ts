e836c89b30f1275539b09d9b477c950250b41315
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-05-31 08:31:34 -0300

    hash password middleware

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,19 +1,40 @@
 import mongoose from 'mongoose'
+import bcryptjs from 'bcryptjs'
 
 const { Schema, model } = mongoose
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-    lowercase: true
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
   },
-  password: {
-    type: String,
-    required: true
+  {
+    versionKey: false,
+    timestamps: true
+  }
+)
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
+
+  try {
+    const salt = await bcryptjs.genSalt(10)
+    const hash = await bcryptjs.hash(this.password, salt)
+    this.password = hash.toString()
+    next()
+  } catch (error) {
+    console.log(error)
+    throw new Error('fallo el hash de contraseña')
   }
 })
 
-export const User = model('user', userSchema)
+export const User = model('User', userSchema)
