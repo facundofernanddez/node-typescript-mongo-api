7890967776d12c7f3343eb41e5eae727fe584863
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-05-31 10:01:50 -0300

    save user in DB and validate if already exist

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -6,17 +6,25 @@ export const login = async (_req: Request, res: Response): Promise<void> => {
 }
 
 export const register = async (req: Request, res: Response): Promise<void> => {
-  console.log(req.body)
   const { email, password } = req.body
   try {
+    // let user = await User.findOne({ email })
+    // if (user !== null) throw { code: 11000 }
+
     const user = new User({ email, password })
+
+    // consultar si existe el email
+
     await user.save()
 
     // jwt token
 
     res.json({ message: 'success' })
     return
-  } catch (error) {
-    console.log(error)
+  } catch (error: any) {
+    console.log(error.code)
+    if (error.code === 11000) {
+      res.status(400).json({ error: 'user already exist' })
+    }
   }
 }
