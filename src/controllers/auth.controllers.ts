8abc35681d6375da1c9b1822c2ecc2c6319261d4
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-05-31 08:33:02 -0300

    save new user registered

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,9 +1,22 @@
 import { Request, Response } from 'express'
+import { User } from '../models/User'
 
 export const login = async (_req: Request, res: Response): Promise<void> => {
   res.json({ message: 'login' })
 }
 
-export const register = async (_req: Request, res: Response): Promise<void> => {
-  res.json({ message: 'register' })
+export const register = async (req: Request, res: Response): Promise<void> => {
+  console.log(req.body)
+  const { email, password } = req.body
+  try {
+    const user = new User({ email, password })
+    await user.save()
+
+    // jwt token
+
+    res.json({ message: 'success' })
+    return
+  } catch (error) {
+    console.log(error)
+  }
 }
