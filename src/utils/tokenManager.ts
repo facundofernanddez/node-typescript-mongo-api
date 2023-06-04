709e03156cf01a2a5580f9ef688018c16831ecc3
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-06-04 17:38:03 -0300

    create refresh token

--- src/utils/tokenManager.ts
+++ src/utils/tokenManager.ts
@@ -1,3 +1,4 @@
+import { Response } from 'express'
 import jwt from 'jsonwebtoken'
 
 interface IToken {
@@ -12,6 +13,20 @@ export const generateToken = (uid: string): IToken | any => {
     const token = jwt.sign({ uid }, process.env.JWT_SECRET as jwt.Secret, { expiresIn })
 
     return { token, expiresIn }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const generateRefreshToken = async (uid: string, res: Response): Promise<void> => {
+  const expiresIn = 60 * 60 * 24 * 30
+  try {
+    const refreshToken = jwt.sign({ uid }, process.env.JWT_REFRESH_TOKEN as jwt.Secret, { expiresIn })
+
+    res.cookie('resfreshtoken', refreshToken, {
+      httpOnly: true,
+      secure: false
+    })
   } catch (error) {
     console.log(error)
   }
