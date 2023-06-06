781446cb5bf7d6551b7d6ec41d2c24b6784762c1
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-06-06 20:53:17 -0300

    refact method requireToken

--- src/middlewares/requireToken.ts
+++ src/middlewares/requireToken.ts
@@ -1,18 +1,18 @@
-import { NextFunction, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
 import { CustomRequest } from '../types/customRequest'
 
 export const requireToken = async (
-  req: CustomRequest,
+  req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
   try {
     const token = req.header('Authorization')?.replace('Bearer ', '')
 
-    const payload = jwt.verify(token as string, process.env.JWT_SECRET as jwt.Secret)
+    const payload = jwt.verify(token as string, process.env.JWT_SECRET as jwt.Secret);
 
-    req.uid = (payload as { uid: string }).uid
+    (req as CustomRequest).uid = (payload as { uid: string }).uid
 
     next()
   } catch (error) {
