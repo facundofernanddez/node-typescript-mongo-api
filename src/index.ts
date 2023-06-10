0fc105e55e8d51e07a60e53359503784061e1036
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-06-10 11:19:58 -0300

    start to configure cors

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,20 @@ import './database/connectdb'
 import authRouter from './routes/auth.routes'
 import cookieParser from 'cookie-parser'
 import linkRouter from './routes/link.routes'
+import cors from 'cors'
 
 const PORT = process.env.PORT !== undefined ? process.env.PORT : 5000
 
 const app = express()
+
+const whiteList = [process.env.ORIGIN1] as string[]
+
+const options: cors.CorsOptions = {
+  origin: whiteList
+}
+
+app.use(cors(options))
+
 app.use(express.json())
 app.use(cookieParser())
 
